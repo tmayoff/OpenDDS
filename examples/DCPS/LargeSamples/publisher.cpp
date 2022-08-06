5b3eb6a004f9b09f0df3e4cc3ef65c55a09add9a
Tyler Mayoff <tyler@tylermayoff.com>
2022-08-06 16:17:15 -0400

    started large sample test

---
+++ examples/DCPS/LargeSamples/publisher.cpp
@@ -0,0 +1,13 @@
+#include "MessengerTypeSupportImpl.h"
+
+#include <ace/Log_Msg.h>
+
+#include <dds/DCPS/Marked_Default_Qos.h>
+#include <dds/DdsDcpsInfrastructureTypeSupportC.h>
+#include <dds/DCPS/Service_Participant.h>
+
+int ACE_TMAIN (int argc, ACE_TCHAR *argv[]) {
+    DDS::DomainParticipantFactory_var dpf = TheParticipantFactoryWithArgs(argc, argv);
+
+    DDS::DomainParticipant_var participant = dpf->create_participant(42, PARTICIPANT_QOS_DEFAULT, 0, OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+}
