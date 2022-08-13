ad9fad1abc0204477bae0e03091f6b2beac5e8b8
Tyler Mayoff <tyler@tylermayoff.com>
2022-08-13 15:59:37 -0400

    Simplified test

---
+++ tests/DCPS/LargeImageSample/DataReaderListener.h
@@ -0,0 +1,61 @@
+/*
+ *
+ *
+ * Distributed under the OpenDDS License.
+ * See: http://www.opendds.org/license.html
+ */
+
+#ifndef DATAREADER_LISTENER_IMPL
+#define DATAREADER_LISTENER_IMPL
+
+#include <dds/DCPS/LocalObject.h>
+#include <dds/DdsDcpsSubscriptionC.h>
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include <map>
+#include <set>
+
+class DataReaderListenerImpl
+  : public virtual OpenDDS::DCPS::LocalObject<DDS::DataReaderListener> {
+public:
+  DataReaderListenerImpl();
+
+  virtual ~DataReaderListenerImpl();
+
+  virtual void on_requested_deadline_missed(
+    DDS::DataReader_ptr reader,
+    const DDS::RequestedDeadlineMissedStatus& status);
+
+  virtual void on_requested_incompatible_qos(
+    DDS::DataReader_ptr reader,
+    const DDS::RequestedIncompatibleQosStatus& status);
+
+  virtual void on_liveliness_changed(
+    DDS::DataReader_ptr reader,
+    const DDS::LivelinessChangedStatus& status);
+
+  virtual void on_subscription_matched(
+    DDS::DataReader_ptr reader,
+    const DDS::SubscriptionMatchedStatus& status);
+
+  virtual void on_sample_rejected(
+    DDS::DataReader_ptr reader,
+    const DDS::SampleRejectedStatus& status);
+
+  virtual void on_data_available(
+    DDS::DataReader_ptr reader);
+
+  virtual void on_sample_lost(
+    DDS::DataReader_ptr reader,
+    const DDS::SampleLostStatus& status);
+
+  bool data_consistent() const;
+
+private:
+  DDS::DataReader_var  reader_;
+};
+
+#endif /* DATAREADER_LISTENER_IMPL  */
